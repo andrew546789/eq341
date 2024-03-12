449cfb56e12cf4e126b92176e3d20f1035d01026
andrew546789 <121363902+andrew546789@users.noreply.github.com>
2024-03-12 11:14:06 -0400

    Add files via upload
    fixed if stuff

--- LuaHeal.lua
+++ LuaHeal.lua
@@ -3,19 +3,20 @@ local mq = require('mq')
 local function main()
 -- put memspellset here
 	local tank = "jabopie"
+	--mq.TLO.target
 	print("starting mac")
 --start following tank here
 
 	--while loop to heal and do stuff
 	while 1 do 
-		if --active combat state
-			if mq.TLO.me.PctMana < 100--i think
+		if mq.TLO.me.CombatState.Equal[ACTIVE] then
+			if mq.TLO.me.PctMana < 100 then
 				rest()
 				end
 			end
-		if mq.TLO.me.PctMana > 10
+		if mq.TLO.me.PctMana > 10 then
 			heal()
-			if (mq.TLO.me.PctMana >= 50) and (mq.TLO.Target.PctHPs >=)
+			if (mq.TLO.me.PctMana >= 50) and (mq.TLO.Target.PctHPs >=) then
 				buff()
 				end
 			mq.delay(3000)
@@ -28,7 +29,7 @@ local function main()
 	
 			
 local function heal()
-	if mq.TLO.Target.PctHPs >= 90
+	if mq.TLO.Target.PctHPs >= 90 then
 		print("health is good. it is" + mq.TLO.Target.PctHPs)
 		end
 	else
@@ -41,7 +42,7 @@ local function rest()
 	--turn follow off
 	--sit
 	while mq.TLO.me.PctMana < 99
-		if (mq.TLO.Target.PctHPs < 50) and (mq.TLO.me.PctMana > 10)
+		if (mq.TLO.Target.PctHPs < 50) and (mq.TLO.me.PctMana > 10) then
 			heal()
 			end
 		mq.delay(3000)
@@ -51,7 +52,7 @@ local function rest()
 	end
 
 local function buff()
-	if (not --${Target.Buff[Strength].ID}-- ) and --${Target.Distance}-- < 30 )
+	if (not mq.TLO.Target.Distance < 30 ) --${Target.Buff[Strength].ID}-- ) and
 		print("buffing")
 		--cast buff
 		end
