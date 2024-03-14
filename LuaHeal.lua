b4f575ff2e6fb25b53e153774dd3a9ea5f9e7034
andrew546789 <121363902+andrew546789@users.noreply.github.com>
2024-03-14 10:48:08 -0400

    Add files via upload
    guh

--- LuaHeal.lua
+++ LuaHeal.lua
@@ -9,59 +9,61 @@ local function main()
 
 	--while loop to heal and do stuff
 	while 1 do 
-		if mq.TLO.me.CombatState.Equal[ACTIVE] then
-			if mq.TLO.me.PctMana < 100 then
+		if mq.TLO.Me.CombatState.Equal[ACTIVE] then
+			if mq.TLO.Me.PctMana() < 100 then
 				rest()
-				end
-			end
-		if mq.TLO.me.PctMana > 10 then
-			heal()
-			if (mq.TLO.me.PctMana >= 50) and (mq.TLO.Target.PctHPs >=) then
-				buff()
-				end
-			mq.delay(3000)
-			end
-		else
-			rest()
 			end
 		end
+		if mq.TLO.Me.PctMana() > 10 then
+			heal()
+			if (mq.TLO.Me.PctMana() >= 50) and (mq.TLO.Target.PctHPs() >= 90) then
+				buff()
+			end
+			mq.delay(3000)
+		else
+			rest()
+		end
 	end
+end
+
 	
 			
 local function heal()
-	if mq.TLO.Target.PctHPs >= 90 then
-		print("health is good. it is" + mq.TLO.Target.PctHPs)
-		end
+	if mq.TLO.Target.PctHPs() >= 90 then
+		print("health is good. it is" + mq.TLO.Target.PctHPs())
 	else
-		print("healing because health is" + mq.TLO.Target.PctHPs)
+		print("healing because health is" + mq.TLO.Target.PctHPs())
 		--cast healing
-		end
 	end
+	return 0
+end
 
 local function rest()
 	--turn follow off
 	--sit
-	while mq.TLO.me.PctMana < 99
-		if (mq.TLO.Target.PctHPs < 50) and (mq.TLO.me.PctMana > 10) then
+	while mq.TLO.Me.PctMana() < 99 do
+		if (mq.TLO.Target.PctHPs() < 50) and (mq.TLO.Me.PctMana() > 10) then
 			heal()
-			end
-		mq.delay(3000)
 		end
+		mq.delay(3000)
+	end
 	--stand
 	--continue following
-	end
+	return 0
+end
 
 local function buff()
-	if (not mq.TLO.Target.Distance < 30 ) --${Target.Buff[Strength].ID}-- ) and
+	if (not mq.TLO.Target.Distance() < 30 ) then --mq.TLO.Target.Buff[Strength].ID--
 		print("buffing")
 		--cast buff
-		end
 	end
+	return 0
+end
 
 local terminate = false
 mq.bind('/assend', function() terminate = true end)
 
-if not terminate
+if not terminate then
     main()
     mq.delay(1000) -- equivalent to '1s'
 end
